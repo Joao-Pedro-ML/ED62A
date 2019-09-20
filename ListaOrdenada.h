4c6b3102e6a823dabbfae6609bab2d73938699db
João Pedro Moreto Lourenção <48949350+Joao-Pedro-ML@users.noreply.github.com>
2019-09-20 14:23:07 -0300

    Update ListaOrdenada.h

--- ListaOrdenada.h
+++ ListaOrdenada.h
@@ -10,138 +10,143 @@ extern "C" {
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct  {
-  int chave;
-} Objeto;
+    typedef struct {
+        int chave;
+    } Objeto;
 
-typedef struct NoLista *PtrNoLista;
+    typedef struct NoLista *PtrNoLista;
 
-typedef struct NoLista {
-    Objeto elemento;
-    PtrNoLista proximo;
-} NoLista;
+    typedef struct NoLista {
+        Objeto elemento;
+        PtrNoLista proximo;
+    } NoLista;
 
-typedef struct {
-    PtrNoLista primeiro;
-    int tamanho;
-} Lista;
+    typedef struct {
+        PtrNoLista primeiro;
+        int tamanho;
+    } Lista;
 
-void iniciaLista(Lista *list){
-    list->primeiro = NULL;
-    list->tamanho = 0;
-}
+    void iniciaLista(Lista *list) {
+        list->primeiro = NULL;
+        list->tamanho = 0;
+    }
 
-void inserir(Lista *list, Objeto x){
-    
-    PtrNoLista novo;
-    novo = (PtrNoLista) malloc(sizeof (NoLista));
-    novo->elemento.chave = x.chave;
-    if(list->tamanho == 0 || x.chave < list->primeiro->elemento.chave){
-        novo->proximo = list->primeiro;
-        list->primeiro = novo;
-        list->tamanho++;
-    } else{
-        PtrNoLista aux;
-        aux = list->primeiro;
-        while(aux->proximo != NULL && x.chave > aux->proximo->elemento.chave){
-            aux = aux->proximo;
+    void inserir(Lista *list, Objeto x) {
+
+        PtrNoLista novo;
+        novo = (PtrNoLista) malloc(sizeof (NoLista));
+        novo->elemento.chave = x.chave;
+        if (list->tamanho == 0 || x.chave < list->primeiro->elemento.chave) {
+            novo->proximo = list->primeiro;
+            list->primeiro = novo;
+            list->tamanho++;
+        } else {
+            PtrNoLista aux;
+            aux = list->primeiro;
+            while (aux->proximo != NULL && x.chave > aux->proximo->elemento.chave) {
+                aux = aux->proximo;
+            }
+            novo->proximo = aux->proximo;
+            aux->proximo = novo;
+            list->tamanho++;
         }
-        novo->proximo = aux->proximo;
-        aux->proximo =novo;
-        list->tamanho++;
     }
-}
 
-void imprimirLista(Lista *list){
-    PtrNoLista aux;
-    printf("\n[");
-    printf("%i", list->primeiro->elemento.chave);
-    for(aux = list->primeiro; aux != NULL; aux = aux->proximo){
-        printf(" %i ", aux->elemento.chave);
+    void imprimirLista(Lista *list) {
+        PtrNoLista aux;
+        printf("\n[");
+        for (aux = list->primeiro; aux != NULL; aux = aux->proximo) {
+            printf(" %i ", aux->elemento.chave);
+        }
+        printf("]\n");
     }
-    printf("]\n");
-}
 
-void destruir(Lista *list){
-    PtrNoLista aux;
-    while(aux!=NULL){
-        aux = list->primeiro;
-        list->primeiro = list->primeiro->proximo;
-        free(aux);
+    void destruir(Lista *list) {
+        PtrNoLista aux;
+        while (aux != NULL) {
+            aux = list->primeiro;
+            list->primeiro = list->primeiro->proximo;
+            free(aux);
+        }
     }
-}
 
-bool estaVazia(Lista *list){
-    return(list->tamanho ==0);
-}
-
-bool pesquisar(Lista *list, Objeto x){
-    if(list->tamanho == 0){
-        return false;
+    bool estaVazia(Lista *list) {
+        return (list->tamanho == 0);
     }
-    PtrNoLista aux;
-    aux = list->primeiro;
-    while(aux != NULL && x.chave > aux->elemento.chave){
-        aux = aux->proximo;
-        if(aux == NULL || aux->elemento.chave > x.chave){
+
+    bool pesquisar(Lista *list, Objeto x) {
+        if (estaVazia(list)) {
             return false;
         }
-        return true;
-    }
-    
-}
-
-int tamanhoLista(Lista *list){
-    return(list->tamanho);
-}
-
-void removeElemento(Lista *list, int chave, Objeto *item){
-    if(list->tamanho == 0 && item->chave < list->primeiro->elemento.chave){
-        printf("Lista vazia!!!\n");
-    } //casos 1 e 2
-    PtrNoLista aux;
-    aux = list->primeiro;
-    while(aux != NULL && item->chave > aux->elemento.chave){
-        aux = aux->proximo;
-       
-    } //casos 4 e 5
-    if(aux == NULL || aux->elemento.chave > item->chave){
-            item->chave = aux->elemento.chave;
-            list->tamanho--;
-        } else{
-            printf("O valor solicitado não existe!!!\n");
+        PtrNoLista aux = list->primeiro;
+        while (aux != NULL && x.chave > aux->elemento.chave) {
+            aux = aux->proximo;
         }
-}
-
-void removePrimeiro(Lista *list, Objeto *item){
-    Objeto x;
-    if(item->chave == list->primeiro->elemento.chave){
-        item->chave = list->primeiro->elemento.chave;
-        list->tamanho--;
-    } //caso 3
-}
-
-void removeUltimo(Lista *list, Objeto *item){
-    PtrNoLista aux;
-    aux = list->primeiro;
-    while(aux != NULL){
-        aux = aux->proximo;
+        if (aux == NULL || aux->elemento.chave > x.chave) {
+            return false;
+        } else {
+            return true;
+        }
     }
-    item->chave = aux->elemento.chave;
-    list->tamanho--;
-    free(aux);
-}
 
-Objeto primeiro(Lista *list){
-    return(list->primeiro->elemento);
-}
-
-Objeto ultimo(Lista *list){
-    Objeto x;
-    if(list->primeiro->proximo->elemento.chave < x.chave && list->primeiro->proximo == NULL){
-        return x;
+    int tamanhoLista(Lista *list) {
+        return (list->tamanho);
     }
-}
+
+    void removePrimeiro(Lista *list, Objeto *item) {
+
+        PtrNoLista aux;
+        if (!estaVazia(list)) {
+            aux = list->primeiro;
+            item->chave = aux->elemento.chave;
+            list->primeiro = list->primeiro->proximo;
+            free(aux);
+            list->tamanho--;
+            
+        } //caso 3
+    }
+
+    void removeElemento(Lista *list, int chave, Objeto *item) {
+        //casos 1 e 2
+        if (estaVazia(list) || chave < list->primeiro->elemento.chave) {
+            printf("Lista vazia ou não existe o elemento na lista\n");
+            return;
+        } else{
+            //se for o primeiro elemento
+            if(chave == list->primeiro->elemento.chave){
+                printf("Removendo o primeiro elemento!\n");
+                removePrimeiro(list, item);
+            } else{
+                PtrNoLista aux = list->primeiro;
+                while((aux->proximo != NULL) && (chave > aux->proximo->elemento.chave)){
+                    aux = aux->proximo;
+                }
+                if((aux->proximo == NULL) || (chave < aux->proximo->elemento.chave)){
+                    printf("O elemento não existe na lista!\n");
+                } else{
+                    PtrNoLista remove = aux->proximo;
+                    aux->proximo = aux->proximo->proximo;
+                    list->tamanho--;
+                    free(remove);
+                }
+            }
+        }
+    }
+
+    Objeto primeiro(Lista *list) {
+        return (list->primeiro->elemento);
+    }
+
+    Objeto ultimo(Lista *list) {
+        Objeto x;
+        PtrNoLista aux = list->primeiro;
+        while (aux != NULL && x.chave > aux->elemento.chave) {
+            aux = aux->proximo;
+            if (list->primeiro->proximo->elemento.chave < x.chave && list->primeiro->proximo == NULL) {
+                return x;
+            }
+        }
+    }
 
 
 
