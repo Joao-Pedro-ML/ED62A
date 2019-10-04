b00dd603d24f1c435ca07e6023b7cd4a7c78cd01
João Pedro Moreto Lourenção <48949350+Joao-Pedro-ML@users.noreply.github.com>
2019-10-04 14:14:34 -0300

    Update BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -11,61 +11,87 @@ extern "C" {
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct{
-    int key;
-}objeto;
+    typedef struct {
+        int key;
+    } objeto;
 
-typedef struct NoTree *PtrNoTree;
+    typedef struct NoTree *PtrNoTree;
 
-typedef struct NoTree{
-    objeto elemento;
-    PtrNoTree left;
-    PtrNoTree right;
-}NoArvore;
+    typedef struct NoTree {
+        objeto elemento;
+        PtrNoTree left;
+        PtrNoTree right;
+    } NoArvore;
 
-void iniciaArvore(PtrNoTree *arvore){
-    *arvore = NULL;
-}
+    void iniciaArvore(PtrNoTree *arvore) {
+        *arvore = NULL;
+    }
 
-bool estaVazia(PtrNoTree *arvore){
-    return (*arvore == NULL);
-}
+    bool estaVazia(PtrNoTree *arvore) {
+        return (*arvore == NULL);
+    }
 
-bool inserir(PtrNoTree *arvore, objeto x){
-    if(*arvore == NULL){
-        (*arvore) = malloc(sizeof(NoArvore));
-        (*arvore)->right = (*arvore)->left = NULL;
-        (*arvore)->elemento = x;
-        return (true);
-    }
-    if((*arvore)->elemento.key == x.key){
-        return (false);
-    }
-    if((*arvore)->elemento.key > x.key){
-        return (inserir(&(*arvore)->left, x));
-    } else{
-        return (inserir(&(*arvore)->right, x));
-    }
-    
-}
+    bool inserir(PtrNoTree *arvore, objeto x) {
+        if (*arvore == NULL) {
+            (*arvore) = malloc(sizeof (NoArvore));
+            (*arvore)->right = (*arvore)->left = NULL;
+            (*arvore)->elemento = x;
+            return (true);
+        }
+        if ((*arvore)->elemento.key == x.key) {
+            return (false);
+        }
+        if ((*arvore)->elemento.key > x.key) {
+            return (inserir(&(*arvore)->left, x));
+        } else {
+            return (inserir(&(*arvore)->right, x));
+        }
 
-bool procurar(PtrNoTree *arvore, int chave, objeto *ret){
-    //nao achou elemento
-    if(*arvore == NULL){
-        return (false);
     }
-    //achou elemento
-    if((*arvore)->elemento.key == chave){
-        return (true);
+
+    bool procurar(PtrNoTree *arvore, int chave, objeto *ret) {
+        //nao achou elemento
+        if (*arvore == NULL) {
+            printf("0\n");
+            return (false);
+        }
+        //achou elemento
+        if ((*arvore)->elemento.key == chave) {
+            printf("1\n");
+            return (true);
+        }
+        if (chave < (*arvore)->elemento.key) {
+            //procurar na subárvore esquerda
+            return (procurar(&(*arvore)->left, chave, ret));
+        } else { //chave > (*arvore)->elemento.key
+            //procurar na subárvore direita
+            return (procurar(&(*arvore)->right, chave, ret));
+        }
     }
-    if(chave < (*arvore)->elemento.key){
-        //procurar na subárvore esquerda
-        return (procurar(&(*arvore)->left, chave, ret));
-    } else{ //chave > (*arvore)->elemento.key
-        //procurar na subárvore direita
-        return (procurar(&(*arvore)->right, chave, ret));
+
+    void preOrdem(PtrNoTree *arvore) {
+        if ((*arvore) != NULL) {
+            printf("%i\n", (*arvore)->elemento.key);
+            preOrdem(&(*arvore)->left);
+            preOrdem(&(*arvore)->right);
+        }
     }
-}
+
+    void posOrdem(PtrNoTree *arvore) {
+        if ((*arvore) != NULL) {
+            posOrdem(&(*arvore)->left);
+            posOrdem(&(*arvore)->right);
+            printf("%i\n", (*arvore)->elemento.key);
+        }
+    }
+
+    void emOrdem(PtrNoTree *arvore) {
+        if ((*arvore) != NULL) {
+            emOrdem(&(*arvore)->left);
+            printf("%i\n", (*arvore)->elemento.key);
+            emOrdem(&(*arvore)->right);
+        }
+    }
 
 #ifdef __cplusplus
 }
